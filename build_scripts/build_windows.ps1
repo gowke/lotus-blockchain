b79984d06ba64f8b1140dc19f625d356525e0f54
Waruta <fgreen5.extra@gmail.com>
2022-08-24 06:43:37 +1000

    Modfiy Scripts for CLI-Only Build

--- build_scripts/build_windows.ps1
+++ build_scripts/build_windows.ps1
@@ -1,4 +1,4 @@
-# $env:path should contain a path to editbin.exe and signtool.exe
+# $env:path should contain a path to editbin.exe
 
 $ErrorActionPreference = "Stop"
 
@@ -6,11 +6,11 @@ mkdir build_scripts\win_build
 
 git status
 
-if (-not (Test-Path env:LOTUS_INSTALLER_VERSION)) {
-  $env:LOTUS_INSTALLER_VERSION = '0.0.0'
-  Write-Output "WARNING: No environment variable LOTUS_INSTALLER_VERSION set. Using 0.0.0"
+if (-not (Test-Path env:LOTUS_VERSION)) {
+  $env:LOTUS_VERSION = '0.0.0'
+  Write-Output "WARNING: No environment variable LOTUS_VERSION set. Using 0.0.0"
 }
-Write-Output "Lotus Version is: $env:LOTUS_INSTALLER_VERSION"
+Write-Output "Lotus Version is: $env:LOTUS_VERSION"
 Write-Output "   ---"
 
 Write-Output "   ---"
@@ -19,100 +19,24 @@ Write-Output "   ---"
 $SPEC_FILE = (python -c 'import lotus; print(lotus.PYINSTALLER_SPEC_PATH)') -join "`n"
 pyinstaller --log-level INFO $SPEC_FILE
 
-Write-Output "   ---"
-Write-Output "Copy lotus executables to lotus-blockchain-gui\"
-Write-Output "   ---"
-Copy-Item "dist\daemon" -Destination "..\lotus-blockchain-gui\packages\gui\" -Recurse
-
-Write-Output "   ---"
-Write-Output "Setup npm packager"
-Write-Output "   ---"
-Set-Location -Path ".\npm_windows" -PassThru
-npm ci
-$Env:Path = $(npm bin) + ";" + $Env:Path
-Set-Location -Path "..\" -PassThru
-
-Set-Location -Path "..\lotus-blockchain-gui" -PassThru
-# We need the code sign cert in the gui subdirectory so we can actually sign the UI package
-If ($env:HAS_SECRET) {
-    Copy-Item "win_code_sign_cert.p12" -Destination "packages\gui\"
-}
-
 git status
 
-Write-Output "   ---"
-Write-Output "Prepare Electron packager"
-Write-Output "   ---"
-$Env:NODE_OPTIONS = "--max-old-space-size=3000"
-
-lerna clean -y
-npm ci
-# Audit fix does not currently work with Lerna. See https://github.com/lerna/lerna/issues/1663
-# npm audit fix
-
-git status
-
-Write-Output "   ---"
-Write-Output "Electron package Windows Installer"
-Write-Output "   ---"
-npm run build
-If ($LastExitCode -gt 0){
-    Throw "npm run build failed!"
-}
-
-# Change to the GUI directory
-Set-Location -Path "packages\gui" -PassThru
+# Change to the CLI directory
+Set-Location -Path "dist\daemon" -PassThru
 
 Write-Output "   ---"
 Write-Output "Increase the stack for lotus command for (lotus plots create) chiapos limitations"
 # editbin.exe needs to be in the path
-editbin.exe /STACK:8000000 daemon\lotus.exe
-Write-Output "   ---"
-
-$packageVersion = "$env:LOTUS_INSTALLER_VERSION"
-$packageName = "Lotus-$packageVersion"
-
-Write-Output "packageName is $packageName"
-
-Write-Output "   ---"
-Write-Output "fix version in package.json"
-choco install jq
-cp package.json package.json.orig
-jq --arg VER "$env:LOTUS_INSTALLER_VERSION" '.version=$VER' package.json > temp.json
-rm package.json
-mv temp.json package.json
-Write-Output "   ---"
-
-Write-Output "   ---"
-Write-Output "electron-packager"
-electron-packager . Lotus --asar.unpack="**\daemon\**" --overwrite --icon=.\src\assets\img\lotus.ico --app-version=$packageVersion
-Write-Output "   ---"
-
-Write-Output "   ---"
-Write-Output "node winstaller.js"
-node winstaller.js
+editbin.exe /STACK:8000000 lotus.exe
 Write-Output "   ---"
 
 git status
 
-If ($env:HAS_SECRET) {
-   Write-Output "   ---"
-   Write-Output "Add timestamp and verify signature"
-   Write-Output "   ---"
-   signtool.exe timestamp /v /t http://timestamp.comodoca.com/ .\release-builds\windows-installer\LotusSetup-$packageVersion.exe
-   signtool.exe verify /v /pa .\release-builds\windows-installer\LotusSetup-$packageVersion.exe
-   }   Else    {
-   Write-Output "Skipping timestamp and verify signatures - no authorization to install certificates"
-}
-
-git status
+Write-Output "   ---"
+Write-Output "Moving final binaries to expected location"
+Write-Output "   ---"
+Copy-Item "." -Destination "$env:GITHUB_WORKSPACE\Lotus-win32-x64" -Recurse
 
 Write-Output "   ---"
-Write-Output "Moving final installers to expected location"
-Write-Output "   ---"
-Copy-Item ".\Lotus-win32-x64" -Destination "$env:GITHUB_WORKSPACE\lotus-blockchain-gui\" -Recurse
-Copy-Item ".\release-builds" -Destination "$env:GITHUB_WORKSPACE\lotus-blockchain-gui\" -Recurse
-
-Write-Output "   ---"
-Write-Output "Windows Installer complete"
+Write-Output "Windows CLI Binaries complete"
 Write-Output "   ---"
